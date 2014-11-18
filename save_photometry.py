5ac943b4ee543564714093477b135cb010b4f944
Marco Rocchetto <rocchetto.marco@gmail.com>
2014-11-18 17:50:11 +0000

    add airmass, and parameter -n to set n of stars to save.

--- save_photometry.py
+++ save_photometry.py
@@ -37,13 +37,15 @@ def main():
     parser.add_option('-d', '--data',
                       dest="print_data",
     )
+    parser.add_option('-n', '--nstar',
+                      dest="n_star",
+    )
 
     options, remainder = parser.parse_args()
 
     param_filename = os.path.abspath(os.path.expanduser(options.param_filename))
     photometry_instance = os.path.abspath(os.path.expanduser(options.photometry_instance))
     output_filename = os.path.abspath(os.path.expanduser(options.output_filename))
-
 
     pars = Parameters(param_filename)
 
@@ -54,33 +56,45 @@ def main():
         logging.error('Cannot load photometry instance. Check argument -i')
         sys.exit()
 
+    nstar = int(options.n_star)
+
+
+    if nstar > photometry.nstar:
+        nstar = photometry.nstar
+
+
     logging.info('Target is %s (RA %s Dec %s), star n. %i in master catalogue' %
                  (photometry.dataset.object.name, photometry.dataset.object.ra, photometry.dataset.object.dec,
                   photometry.dataset.targetid))
 
     if options.print_data == 'mag':
         bjd = np.asarray(photometry.dataset.bjd)
+        airmass = np.asarray(photometry.dataset.airmass)
         mag = np.nan_to_num(photometry.aperture[float(options.aperture_size)].mag)
         #np.savetxt(output_filename, np.column_stack((bjd,mag)), fmt='%.7e')
 
         f = open(output_filename, 'wb')
         for n in range(photometry.nobs):
-            line = '%.13e' % bjd[n]
-            for i in range(3):
-                line += ' %.7e' % mag[n, i]
+            line = '%13.5f' % bjd[n]
+            line += ' %.5f' % airmass[n]
+            for i in range(nstar):
+                line += ' %.5f' % mag[n, i]
             f.write(line + '\n')
         f.close()
 
     if options.print_data == 'mag_err':
         bjd = np.asarray(photometry.dataset.bjd)
-        mag = np.nan_to_num(photometry.aperture[float(options.aperture_size)].mag[:,:3])
-        sigmag = np.nan_to_num(photometry.aperture[float(options.aperture_size)].sigmag[:,:3])
+        airmass = np.asarray(photometry.dataset.airmass)
+        mag = np.nan_to_num(photometry.aperture[float(options.aperture_size)].mag)
+        sigmag = np.nan_to_num(photometry.aperture[float(options.aperture_size)].sigmag)
 
         f = open(output_filename, 'wb')
         for n in range(photometry.nobs):
-            line = '%.13e' % bjd[n]
-            for i in range(3):
-                line += ' %.7e %.7e' % (mag[n, i], sigmag[n, i])
+            line = '%13.5f' % bjd[n]
+            line += ' %.5f' % airmass[n]
+
+            for i in range(nstar):
+                line += ' %.5f %.5f' % (mag[n, i], sigmag[n, i])
             f.write(line + '\n')
         f.close()
 
